7050a7014fb4106e4b2d3d68ae393da7c228f063
hoanglong040800 <hoanglong040800@gmail.com>
2021-02-26 23:07:30 +0700

    test husky

--- src/index.js
+++ src/index.js
@@ -1,41 +1,39 @@
-const express = require('express')
-const { urlencoded, json, static } = require('express')
-const morgan = require('morgan')
-const exphbs = require('express-handlebars')
-const path = require('path')
+const express = require('express');
+const { urlencoded, json, static } = require('express');
+const morgan = require('morgan');
+const exphbs = require('express-handlebars');
+const path = require('path');
 
-const route = require('./routes/route')
+const route = require('./routes/route');
 
-const port = 3000
-const app = express()
-
+const port = 3000;
+const app = express();
 
 // HTTP logger
-app.use(morgan('combined'))
-
+app.use(morgan('combined'));
 
 // Middleware
-app.use(urlencoded())
-app.use(json())
-
+app.use(urlencoded());
+app.use(json());
 
 // Template engine
-app.engine('hbs', exphbs({
-  defaultLayout: 'main',
-  extname: '.hbs'
-}))
+app.engine(
+  'hbs',
+  exphbs({
+    defaultLayout: 'main',
+    extname: '.hbs',
+  }),
+);
 app.set('view engine', 'hbs');
-console.log(__dirname)
-app.set('views', path.join(__dirname, 'views'))
+console.log(__dirname);
+app.set('views', path.join(__dirname, 'views'));
 
-
-// Static 
-app.use(static(path.join(__dirname, 'public')))
-
+// Static
+  app.use(static(path.join(__dirname, 'public')));
 
 // Routing
-route(app)
+route(app);
 
-app.listen(port, () => {
-  console.log(`\n[LINK] ------- http://localhost:${port}\n`)
-})
+      app.listen(port, () => {
+        console.log(`\n[LINK] ------- http://localhost:${port}\n`);
+          });

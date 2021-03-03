ce3263ac4ec0299dfcb364fbf47a6d7226ab6ecc
hoanglong040800 <hoanglong040800@gmail.com>
2021-03-03 23:46:02 +0700

    query count

--- src/controllers/MeController.js
+++ src/controllers/MeController.js
@@ -5,19 +5,25 @@ const { multiMongooseToObject } = require('../utils/dbHelper')
 class MeController {
   // [GET] /me/stored/courses
   storedCourses(req, res, next) {
-    Course.find()
-      .then(arr =>
-        res.render('me/stored-courses', { courses: multiMongooseToObject(arr) })
+    Promise.all([Course.find(), Course.count(), Course.countDeleted()])
+      .then(([arr, count, countDeleted]) =>
+        res.render('me/stored-courses', {
+          courses: multiMongooseToObject(arr),
+          count,
+          countDeleted,
+        })
       )
       .catch(next)
   }
 
   // [GET] /me/deleted/courses
   deletedCourses(req, res, next) {
-    Course.findDeleted()
-      .then(arr =>
+    Promise.all([Course.findDeleted(), Course.count(), Course.countDeleted()])
+      .then(([arr, count, countDeleted]) =>
         res.render('me/deleted-courses', {
           courses: multiMongooseToObject(arr),
+          count,
+          countDeleted,
         })
       )
       .catch(next)

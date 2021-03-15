00e3057b24c23a8c442a7ad12fabcc1769ffdde8
hoanglong040800 <hoanglong040800@gmail.com>
2021-03-15 11:22:12 +0700

    auto increment id

--- src/models/Course.js
+++ src/models/Course.js
@@ -1,17 +1,20 @@
 const mongoose = require('mongoose')
 const mongoose_delete = require('mongoose-delete')
 const slug = require('mongoose-slug-generator')
+const AutoIncrement = require('mongoose-sequence')(mongoose)
 
 const Schema = mongoose.Schema
 
 const CourseSchema = new Schema(
   {
+    _id: { type: Number },
     name: { type: String, maxLength: 255 },
     description: { type: String, maxLength: 500 },
     thumbnail: { type: String },
     slug: { type: String, slug: 'name', unique: true },
   },
   {
+    _id: false,
     timestamps: true,
   }
 )
@@ -22,6 +25,7 @@ CourseSchema.plugin(mongoose_delete, {
   overrideMethods: 'all',
   deletedAt: true,
 })
+CourseSchema.plugin(AutoIncrement)
 
 // Tên model trùng với tên docs trong MongoDB
 module.exports = mongoose.model('courses', CourseSchema)
